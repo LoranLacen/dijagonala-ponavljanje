a1f1eddf99f663d6aab2e609bdf1a1d1def1ff0d
LoranLacen <58933094+LoranLacen@users.noreply.github.com>
2019-12-17 14:04:49 +0100

    Add files via upload

---
+++ vjezba za ispit.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int n;
+	int i,j;
+	cout<<"unesi jedan cijeli broj veci od 2 i manji od 100"<<endl;
+	cin>>n;
+	int  A[4][4]{ {1,2,3,4},
+				  {5,6,7,8},
+				  {9,10,11,12},   
+	              {13,14,15,16},
+				  }
+				  
+	for (i=0;i>2;i++){
+	}
+	for (j=0;j<100;i++)
+	cout<<x[i][j]<<" ";
+	}
+	cout<<endl;
+	          
+	
+system ("pause");
+return 0;
+}
